b88b3f419d84c69916e6761650e930df0924c04b
sipoenokem17 <mabdullahofc@gmail.com>
2026-06-07 19:07:07 +0700

    update dashboard

--- menu/dashboard.php
+++ menu/dashboard.php
@@ -15,41 +15,65 @@ $totalStok = mysqli_fetch_assoc(mysqli_query($conn, "
     SELECT SUM(stok) as total
     FROM barang
 "));
+// TOTAL PROFIT
+$profit = mysqli_fetch_assoc(mysqli_query($conn, "
+    SELECT
+        SUM(total_profit) as total_profit
+    FROM transaksi
+    WHERE MONTH(created_at)=MONTH(CURDATE())
+    AND YEAR(created_at)=YEAR(CURDATE())
+
+"));
 // TOTAL BARANG MASUK
 $totalMasuk = mysqli_fetch_assoc(mysqli_query($conn, "
     SELECT SUM(qty) as total
     FROM barang_masuk
 "));
+
+// <div class="col-12 col-sm-6 col-md-3">
+// <div class="info-box mb-3">
+// <span class="info-box-icon bg-success elevation-1">
+// <i class="fas fa-truck"></i>
+// </span>
+// <div class="info-box-content">
+// <span class="info-box-text">
+// Barang Masuk
+// </span>
+// <span class="info-box-number">
+// number_format($totalMasuk['total']); 
+// </span>
+// </div>
+// </div>
+// </div>
+
 // TOTAL TRANSAKSI
 $totalKeluar = mysqli_fetch_assoc(mysqli_query($conn, "
-    SELECT COUNT(*) as total
-    FROM transaksi
+SELECT COUNT(*) as total
+FROM transaksi
 "));
 // ==============================
 // STOK RENDAH
 // ==============================
 $stokRendah = mysqli_query($conn, "
-    SELECT 
-        nama_barang,
-        stok
-    FROM barang
-    WHERE stok <= 5
+SELECT
+nama_barang,
+stok
+FROM barang
+WHERE stok <= 5
     ORDER BY stok ASC
-    LIMIT 7
-");
+    LIMIT 7 ");
 // ==============================
 // STOK BY KATEGORI
 // ==============================
 $kategori = mysqli_query($conn, "
-    SELECT 
-        k.nama_kategori,
-        SUM(b.stok) as total_stok,
-        k.status
-    FROM barang b 
+    SELECT
+    k.nama_kategori,
+    SUM(b.stok) as total_stok,
+    k.status
+    FROM barang b
     LEFT JOIN kategori k
-    ON b.id_kategori = k.id_kategori
-    GROUP BY b.id_kategori
-");
+    ON b.id_kategori=k.id_kategori
+    GROUP BY b.id_kategori ");
 $kategoriLabel = [];
 $kategoriData  = [];
 while ($k = mysqli_fetch_assoc($kategori)) {
@@ -60,15 +84,14 @@ while ($k = mysqli_fetch_assoc($kategori)) {
 // STOK BY SUPPLIER
 // ==============================
 $supplier = mysqli_query($conn, "
-    SELECT 
-        s.nama_supplier,
-        SUM(b.stok) as total_stok,
-        s.status
-    FROM barang b 
+    SELECT
+    s.nama_supplier,
+    SUM(b.stok) as total_stok,
+    s.status
+    FROM barang b
     LEFT JOIN supplier s
-    ON b.id_supplier = s.id_supplier
-    GROUP BY b.id_supplier
-");
+    ON b.id_supplier=s.id_supplier
+    GROUP BY b.id_supplier ");
 $supplierLabel = [];
 $supplierData  = [];
 while ($s = mysqli_fetch_assoc($supplier)) {
@@ -77,7 +100,7 @@ while ($s = mysqli_fetch_assoc($supplier)) {
 }
 ?>
 <!-- CONTENT HEADER -->
-<div class="content-header">
+<div class=" content-header">
     <div class="container-fluid">
         <!-- INFO BOX -->
         <div class="row mt-4">
@@ -113,17 +136,25 @@ while ($s = mysqli_fetch_assoc($supplier)) {
             </div>
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="info-box mb-3">
+
                     <span class="info-box-icon bg-success elevation-1">
-                        <i class="fas fa-truck"></i>
+                        <i class="fas fa-chart-line"></i>
                     </span>
+
                     <div class="info-box-content">
+
                         <span class="info-box-text">
-                            Barang Masuk
+                            Total Profit Bulan ini
                         </span>
+
                         <span class="info-box-number">
-                            <?= number_format($totalMasuk['total']); ?>
+
+                            Rp <?= number_format($profit['total_profit'] ?? 0, 0, ',', '.'); ?>
+
                         </span>
+
                     </div>
+
                 </div>
             </div>
             <div class="col-12 col-sm-6 col-md-3">
@@ -143,208 +174,142 @@ while ($s = mysqli_fetch_assoc($supplier)) {
             </div>
         </div>
     </div>
-</div>
-<!-- CONTENT -->
-<div class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <!-- STOK RENDAH -->
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-header bg-danger border-0">
-                        <h3 class="card-title">
-                            Stok Rendah
-                        </h3>
-                    </div>
-                    <div class="card-body p-0" style="height:300px;">
-                        <table class="table table-sm table-striped mb-0">
-                            <thead>
-                                <tr>
-                                    <th>Barang</th>
-                                    <th width="30%" class="text-center">
-                                        Stok
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php while ($sr = mysqli_fetch_assoc($stokRendah)) { ?>
+    </div>
+    <!-- CONTENT -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- STOK RENDAH -->
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-header bg-danger border-0">
+                            <h3 class="card-title">
+                                Stok Rendah
+                            </h3>
+                        </div>
+                        <div class="card-body p-0" style="height:300px;">
+                            <table class="table table-sm table-striped mb-0">
+                                <thead>
                                     <tr>
-                                        <td>
-                                            <?= $sr['nama_barang']; ?>
-                                        </td>
-                                        <td class="text-center">
-                                            <span class="badge badge-danger">
-                                                <?= $sr['stok']; ?>
-                                            </span>
-                                        </td>
+                                        <th>Barang</th>
+                                        <th width="30%" class="text-center">
+                                            Stok
+                                        </th>
                                     </tr>
-                                <?php } ?>
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    <?php while ($sr = mysqli_fetch_assoc($stokRendah)) { ?>
+                                        <tr>
+                                            <td>
+                                                <?= $sr['nama_barang']; ?>
+                                            </td>
+                                            <td class="text-center">
+                                                <span class="badge badge-danger">
+                                                    <?= $sr['stok']; ?>
+                                                </span>
+                                            </td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- CHART KATEGORI -->
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-header bg-info border-0">
-                        <h3 class="card-title">
-                            Stok by Kategori
-                        </h3>
-                    </div>
-                    <div class="card-body" style="height:300px;">
-                        <canvas id="chartKategori"
-                            height="230"></canvas>
+                <!-- CHART KATEGORI -->
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-header bg-info border-0">
+                            <h3 class="card-title">
+                                Stok by Kategori
+                            </h3>
+                        </div>
+                        <div class="card-body" style="height:300px;">
+                            <canvas id="chartKategori"
+                                height="230"></canvas>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- CHART SUPPLIER -->
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-header bg-secondary border-0">
-                        <h3 class="card-title">
-                            Stok By Supplier
-                        </h3>
-                    </div>
-                    <div class="card-body" style="height:300px;">
-                        <canvas id="chartSupplier"
-                            height="230"></canvas>
+                <!-- CHART SUPPLIER -->
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-header bg-secondary border-0">
+                            <h3 class="card-title">
+                                Stok By Supplier
+                            </h3>
+                        </div>
+                        <div class="card-body" style="height:300px;">
+                            <canvas id="chartSupplier"
+                                height="230"></canvas>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<!-- CHART JS -->
+    <!-- CHART JS -->
 
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-<script>
+    <script>
+        Chart.defaults.font.family = 'Poppins';
+        Chart.defaults.font.size = 12;
 
-    Chart.defaults.font.family = 'Poppins';
-    Chart.defaults.font.size = 12;
+        // ==========================
+        // PIE KATEGORI
+        // ==========================
 
-    // ==========================
-    // PIE KATEGORI
-    // ==========================
+        var kategoriCtx = document
+            .getElementById('chartKategori');
 
-    var kategoriCtx = document
-        .getElementById('chartKategori');
+        new Chart(kategoriCtx, {
 
-    new Chart(kategoriCtx, {
+            type: 'pie',
 
-        type: 'pie',
+            data: {
 
-        data: {
+                labels: <?= json_encode($kategoriLabel); ?>,
 
-            labels: <?= json_encode($kategoriLabel); ?>,
+                datasets: [{
 
-            datasets: [{
+                    data: <?= json_encode($kategoriData); ?>,
 
-                data: <?= json_encode($kategoriData); ?>,
+                    backgroundColor: [
 
-                backgroundColor: [
+                        '#17a2b8',
+                        '#28a745',
+                        '#ffc107',
+                        '#dc3545',
+                        '#6f42c1',
+                        '#fd7e14',
+                        '#20c997'
 
-                    '#17a2b8',
-                    '#28a745',
-                    '#ffc107',
-                    '#dc3545',
-                    '#6f42c1',
-                    '#fd7e14',
-                    '#20c997'
+                    ],
 
-                ],
+                    borderWidth: 2,
+                    borderColor: '#fff'
 
-                borderWidth: 2,
-                borderColor: '#fff'
+                }]
 
-            }]
+            },
 
-        },
+            options: {
 
-        options: {
+                responsive: true,
+                maintainAspectRatio: false,
 
-            responsive: true,
-            maintainAspectRatio: false,
+                plugins: {
 
-            plugins: {
+                    legend: {
 
-                legend: {
+                        position: 'bottom',
 
-                    position: 'bottom',
+                        labels: {
 
-                    labels: {
+                            padding: 15,
+                            usePointStyle: true
 
-                        padding: 15,
-                        usePointStyle: true
-
-                    }
-
-                }
-
-            }
-
-        }
-
-    });
-
-    // ==========================
-    // DONUT SUPPLIER
-    // ==========================
-
-    var supplierCtx = document
-        .getElementById('chartSupplier');
-
-    new Chart(supplierCtx, {
-
-        type: 'doughnut',
-
-        data: {
-
-            labels: <?= json_encode($supplierLabel); ?>,
-
-            datasets: [{
-
-                data: <?= json_encode($supplierData); ?>,
-
-                backgroundColor: [
-
-                    '#6c757d',
-                    '#17a2b8',
-                    '#28a745',
-                    '#ffc107',
-                    '#dc3545',
-                    '#6610f2',
-                    '#fd7e14'
-
-                ],
-
-                borderWidth: 2,
-                borderColor: '#fff',
-                hoverOffset: 12
-
-            }]
-
-        },
-
-        options: {
-
-            responsive: true,
-            maintainAspectRatio: false,
-
-            cutoutPercentage: 60,
-
-            plugins: {
-
-                legend: {
-
-                    position: 'bottom',
-
-                    labels: {
-
-                        padding: 15,
-                        usePointStyle: true
+                        }
 
                     }
 
@@ -352,8 +317,72 @@ while ($s = mysqli_fetch_assoc($supplier)) {
 
             }
 
-        }
+        });
 
-    });
+        // ==========================
+        // DONUT SUPPLIER
+        // ==========================
 
-</script>
+        var supplierCtx = document
+            .getElementById('chartSupplier');
+
+        new Chart(supplierCtx, {
+
+            type: 'doughnut',
+
+            data: {
+
+                labels: <?= json_encode($supplierLabel); ?>,
+
+                datasets: [{
+
+                    data: <?= json_encode($supplierData); ?>,
+
+                    backgroundColor: [
+
+                        '#6c757d',
+                        '#17a2b8',
+                        '#28a745',
+                        '#ffc107',
+                        '#dc3545',
+                        '#6610f2',
+                        '#fd7e14'
+
+                    ],
+
+                    borderWidth: 2,
+                    borderColor: '#fff',
+                    hoverOffset: 12
+
+                }]
+
+            },
+
+            options: {
+
+                responsive: true,
+                maintainAspectRatio: false,
+
+                cutoutPercentage: 60,
+
+                plugins: {
+
+                    legend: {
+
+                        position: 'bottom',
+
+                        labels: {
+
+                            padding: 15,
+                            usePointStyle: true
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        });
+    </script>
